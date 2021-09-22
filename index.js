04a706a1045bd2178803ea2a4e0349f153bca5f5
christinemkhachen <christinemkhachen@gmail.com>
2021-09-22 17:42:15 -0400

    Done.

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ const punctuation = "!";
 const welcomeMessage = "Welcome to Flatbook, " + currentUser + punctuation;
 const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + punctuation;
 const shortGreeting = "Welcome, " + currentUser[0] + punctuation;
+//other correct options
+//const shortGreeting = "Welcome, " + currentUser.slice(0,1) + punctuation;
+//const shortGreeting = "Welcome, " + currentUser.charAt(0) + punctuation;
 
-
+//Interpolation
+//const interpolatedWelcomeMessage = `Welcome to Flatbook,  ${currentUser}${punctuation}`;
+//const interpolatedShortGreeting = `Welcome, ${currentUser[0]}${punctuation}`;
 
 
 
